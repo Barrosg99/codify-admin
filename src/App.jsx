1e2e5dc91f718c8a87b095ae10c2199a08751642
Gabriel Barros <barros.fontes@unesp.br>
2021-02-03 17:24:25 -0300

    adding authProvider

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-import * as React from "react";
-import { Admin } from 'react-admin';
+/* eslint-disable react/jsx-indent */
+/* eslint-disable indent */
+import * as React from 'react';
+import { Admin, Resource, ListGuesser } from 'react-admin';
+import jsonServerProvider from 'ra-data-json-server';
+
+import { authProvider } from './providers';
 
 const dataProvider = jsonServerProvider('https://jsonplaceholder.typicode.com');
 
-const App = () => <Admin dataProvider={dataProvider} />;
+const App = () => (
+	<Admin authProvider={authProvider} dataProvider={dataProvider}>
+		<Resource name="users" list={ListGuesser} />
+	</Admin>
+);
 
 export default App;
---
+++ src/providers/authProvider.js
@@ -0,0 +1,41 @@
+/* eslint-disable prefer-promise-reject-errors */
+import axios from 'axios';
+
+const authProvider = {
+	// authentication
+	login: ({ username, password }) => axios
+		.post(`${process.env.REACT_APP_URL_API}/admin/login`, { username, password })
+		.then(({ data }) => {
+			localStorage.setItem('auth', data.token);
+		})
+		.catch((err) => { throw new Error(err.response.data.error); }),
+	checkError: (error) => {
+		const { status } = error;
+		if (status === 401 || status === 403) {
+			localStorage.removeItem('auth');
+			return Promise.reject({ message: 'Authentication token is no longer valid, please login again' });
+		}
+		return Promise.resolve();
+	},
+	checkAuth: () => (localStorage.getItem('auth')
+		? Promise.resolve()
+		: Promise.reject()),
+	logout: () => {
+		const token = localStorage.getItem('auth');
+		if (!token) return Promise.resolve();
+		return axios
+			.post(`${process.env.REACT_APP_URL_API}/admin/logout`, {}, { headers: { Authorization: `Bearer ${token}` } })
+			.then(() => localStorage.removeItem('auth'))
+			.catch((err) => { throw new Error(err.response.data.error); });
+	},
+	getIdentity: () => {
+		const fullName = 'Admin';
+		const avatar = 'https://i.redd.it/6nho1rhmo2d51.jpg';
+		return Promise.resolve({ fullName, avatar });
+	},
+	// authorization
+	getPermissions: () => Promise.resolve(),
+
+};
+
+export default authProvider;

f26522afc9a44478aa1ea84ccaa30e3c7477485d
Gabriel Barros <barros.fontes@unesp.br>
2021-02-11 13:12:25 -0300

    adding create summary

---
+++ src/resources/summaries/index.js
@@ -0,0 +1,11 @@
+import ListSummaries from './List';
+import CreateSummaries from './Create';
+import ShowSummaries from './Show';
+import EditSummaries from './Edit';
+
+export {
+  ListSummaries,
+  CreateSummaries,
+  ShowSummaries,
+  EditSummaries,
+};

9f20601c439f9b798b0c195c900d4ebd1ba7012e
Gabriel Barros <barros.fontes@unesp.br>
2021-02-11 22:57:19 -0300

    refactoring topics managing

---
+++ src/resources/topics/List.jsx
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import {
+  Datagrid, DateField, List, NumberField, ReferenceField, TextField,
+} from 'react-admin';
+
+export default function TopicsList(props) {
+  return (
+    <List {...props}>
+      <Datagrid rowClick="edit">
+        <TextField source="id" />
+        <ReferenceField source="chapterId" label="Capítulo" reference="chapters">
+          <TextField source="name" />
+        </ReferenceField>
+        <TextField source="name" label="Tópico" />
+        <NumberField source="order" label="Ordem de apresentação" />
+        <DateField source="createdAt" label="Data de criação" />
+        <DateField source="updatedAt" label="Data de alteração" />
+      </Datagrid>
+    </List>
+  );
+}

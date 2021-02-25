cc4829ea63d8d6162e6f80768e327b2acb5b6165
Lucas Murakami <lkenjim98@gmail.com>
2021-02-25 17:20:54 -0300

    feat: showing exercise info with new code fields

--- src/resources/exercises/Show.jsx
+++ src/resources/exercises/Show.jsx
@@ -1,20 +1,39 @@
 import React from 'react';
 import {
-  DateField, ReferenceField, Show, SimpleShowLayout, TextField,
+  DateField, ReferenceField, Show, TextField, TabbedShowLayout, Tab,
 } from 'react-admin';
 
 export default function ExerciseShow(props) {
   return (
     <Show {...props} title={`Exercício #${props.id}`}>
-      <SimpleShowLayout>
-        <TextField source="id" />
-        <ReferenceField source="topicId" label="Tópico" reference="topics">
-          <TextField source="name" />
-        </ReferenceField>
-        <TextField source="description" label="Descrição" />
-        <DateField source="createdAt" label="Data de criação" />
-        <DateField source="updatedAt" label="Data de alteração" />
-      </SimpleShowLayout>
+      <TabbedShowLayout>
+        <Tab label="Identificação">
+          <TextField source="id" />
+
+          <ReferenceField source="topicId" label="Tópico" reference="topics">
+            <TextField source="name" />
+          </ReferenceField>
+
+          <DateField source="createdAt" label="Data de criação" />
+          <DateField source="updatedAt" label="Data de alteração" />
+        </Tab>
+
+        <Tab label="enunciado">
+          <TextField source="enunciated" label="Enunciado" />
+        </Tab>
+
+        <Tab label="Código inicial">
+          <TextField source="initialCode" label="Código inicial" />
+        </Tab>
+
+        <Tab label="Código de Testes">
+          <TextField source="testCode" label="Código de testes" />
+        </Tab>
+
+        <Tab label="Solução">
+          <TextField source="solution" label="Solução" />
+        </Tab>
+      </TabbedShowLayout>
     </Show>
   );
 }
